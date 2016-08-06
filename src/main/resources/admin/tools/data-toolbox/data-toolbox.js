29ff1c6fa6906aea9e87d586c59e93f03f3a1736
rcd-systems <comptepoubelle8@gmail.com>
2016-08-06 13:10:08 +0200

    Admin tool base

--- src/main/resources/admin/tools/data-toolbox/data-toolbox.js
+++ src/main/resources/admin/tools/data-toolbox/data-toolbox.js
@@ -1,6 +1,30 @@
+var mustacheLib = require('/lib/xp/mustache');
+
 exports.get = function (req) {
+
+
+    if (req.params.action) {
+        switch (req.params.action) {
+        case "render":
+            return render(req.params.view);
+        }
+    } else {
+        return render("main")
+    }
+
     return {
-        body: '<html><head></head><body><h1>My first tool</h1></body></html>',
+        status: 400
+    };
+
+
+};
+
+function render(viewName) {
+    var view = resolve(viewName + ".html"); //TODO Secu
+    var body = mustacheLib.render(view, {});
+
+    return {
+        body: body,
         contentType: 'text/html'
     };
-};
+}
