ac2c057732d169a4645dd3eb4f47df46b263770e
rcd-systems <comptepoubelle8@gmail.com>
2016-08-07 13:31:34 +0200

    Dump listing

--- src/main/java/systems/rcd/enonic/datatoolbox/RcdDumpScriptBean.java
+++ src/main/java/systems/rcd/enonic/datatoolbox/RcdDumpScriptBean.java
@@ -1,5 +1,12 @@
 package systems.rcd.enonic.datatoolbox;
 
+import java.nio.file.Path;
+
+import systems.rcd.fwk.core.format.json.RcdJsonService;
+import systems.rcd.fwk.core.format.json.data.RcdJsonArray;
+import systems.rcd.fwk.core.io.file.RcdFileService;
+
+import com.enonic.xp.home.HomeDir;
 import com.enonic.xp.script.bean.BeanContext;
 import com.enonic.xp.script.bean.ScriptBean;
 
@@ -14,6 +21,19 @@ public class RcdDumpScriptBean
 
     public String list()
     {
-        return "[\"a\"]";
+        final RcdJsonArray result = RcdJsonService.createJsonArray();
+        result.add( "sss" );
+
+        final Path dumpDirectoryPath = HomeDir.get().
+            toFile().
+            toPath().
+            resolve( "data/dump" );
+
+        if ( dumpDirectoryPath.toFile().exists() )
+        {
+
+            RcdFileService.listSubPaths( dumpDirectoryPath, dumpPath -> System.out.println( dumpPath ) );
+        }
+        return RcdJsonService.toString( result );
     }
 }
