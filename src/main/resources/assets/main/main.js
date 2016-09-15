af3228a1f69b4ae7a9b74e7c5ae5b5512ec80cf0
rcd-systems <comptepoubelle8@gmail.com>
2016-09-15 23:44:36 +0200

    Table and card display

--- src/main/resources/assets/main/main.js
+++ src/main/resources/assets/main/main.js
@@ -1,16 +1,11 @@
 $('#navLinkExports').click(displayExportsView);
 
-$('#dumpAddButton').click(function () {
-    var dumpName = $('#dumpNameInput').val() || 'dump-' + new Date().toISOString();
-    $.ajax({
-        method: 'POST',
-        url: '/api/system/dump',
-        data: JSON.stringify({name: dumpName}),
-        contentType: 'application/json; charset=utf-8'
-    });
-    displayDumpView();
-});
+$('#actionCreateDump').click(function () {
+    //var dumpName = $('#dumpNameInput').val() || 'dump-' + new Date().toISOString();
+    var dumpName = 'dump-' + new Date().toISOString();
+    createDump(dumpName);
 
+});
 
 function displayExportsView() {
     $.ajax({
@@ -20,12 +15,23 @@ function displayExportsView() {
         var tableBody = dumps.map(createDumpRow);
         $('#dumpTableBody').html(tableBody);
 
-        $('.deleteDumpButton').click(function (event) {
-            deleteDump(event.target.value);
+        $('.action-select-dump').click(function (event) {
+            var index = event.target.getAttribute('index');
+            selectDumpRow(index);
         });
     }).always(function () {
         displayView('viewExports', 'Exports & Dumps');
     });
+}
+
+function createDump(dumpName) {
+    $.ajax({
+        method: 'POST',
+        url: '/api/system/dump',
+        data: JSON.stringify({name: dumpName}),
+        contentType: 'application/json; charset=utf-8'
+    });
+    displayDumpView();
 }
 
 function deleteDump(dumpName) {
@@ -39,14 +45,29 @@ function deleteDump(dumpName) {
     });
 }
 
-function createDumpRow(dump) {
-    return '<div class="table-row">' +
-           '<div class="table-cell button-column"><i class="material-icons checkbox">check_box_outline_blank</i></div>' +
+function selectDumpRow(index) {
+    var row = $('#rowDump' + index);
+    var checkbox = $('#checkboxDump' + index);
+
+    if (row.hasClass('selected')) {
+        row.removeClass('selected');
+        checkbox.removeClass('selected').
+            html('check_box_outline_blank');
+    } else {
+        row.addClass('selected');
+        checkbox.addClass('selected').
+            html('check_box');
+    }
+}
+
+function createDumpRow(dump, index) {
+    return '<div class="table-row" id="rowDump' + index + '">' +
+           '<div class="table-cell button-column"><i class="material-icons checkbox action-select-dump" id="checkboxDump' + index +
+           '" index="' + index + '">check_box_outline_blank</i>' + '</div>' +
            '<div class="table-cell name-column"><span>' + dump.name + '</span></div>' +
            '<div class="table-cell"><span>' + new Date(dump.timestamp).toISOString() + '</span></div>' +
            '</div>';
 }
-
 
 function displayView(viewId, title) {
     $('.view').addClass('rcd-hidden');
