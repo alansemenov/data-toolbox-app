3e1e5416f74f92850b84e64026c85af115efa1f3
Glenn Ricaud <gri@enonic.com>
2016-11-05 00:48:31 +0100

    Icon & descriptions modification

--- src/main/resources/assets/js/data-toolbox-presentation.js
+++ src/main/resources/assets/js/data-toolbox-presentation.js
@@ -1,7 +1,8 @@
 function createPresentationView() {
     //Creates and appends the presentation view
     var presentationViewPathElements = [{name: 'Data Toolbox'}];
-    var presentationViewDescription = 'Data toolbox provides a web interface to visualize and manipulate your Enonic XP data: ' +
-                                      'dump & load your data, take & restore a snapshot, ...';
+    var presentationViewDescription = 'Data toolbox goal is to provide a web interface to visualize and manipulate your Enonic XP data. ' +
+                                      'This tool focuses for the moment on 2 essential Enonic XP capabilities: dumps and snapshots. ' +
+                                      'A widget is also included to allow to export content directly from the Content studio.';
     return new RcdMaterialView('presentation', presentationViewPathElements, presentationViewDescription).init();
 }
