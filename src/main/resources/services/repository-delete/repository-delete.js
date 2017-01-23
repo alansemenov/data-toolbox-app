9b487fa58d100130af6805e4d2fef18af8e0d81b
Glenn Ricaud <gri@enonic.com>
2017-01-24 00:32:57 +0100

    Repositories view

--- src/main/resources/services/repository-delete/repository-delete.js
+++ src/main/resources/services/repository-delete/repository-delete.js
@@ -1,9 +1,49 @@
+var repoLib = require('/lib/xp/repo');
+
 exports.post = function (req) {
-    var bean = __.newBean('systems.rcd.enonic.datatoolbox.RcdRepositoryScriptBean');
     var repositoryNames = JSON.parse(req.body).repositoryNames;
+
+    var result;
+    if (repositoryNames.some(isProtectedAgainstDeletion)) {
+        result = {
+            error: 'The repositories [cms-repo] and [system-repo] cannot be deleted.'
+        };
+    } else {
+        result = runSafely(deleteRepositories, repositoryNames);
+    }
+    return {
+        contentType: 'application/json',
+        body: result
+    }
+
 
     return {
         contentType: 'application/json',
         body: bean.delete(repositoryNames)
     }
 };
+
+function isProtectedAgainstDeletion(repositoryName) {
+    return "cms-repo" === repositoryName || "system-repo" === repositoryName;
+}
+
+function deleteRepositories(repositoryNames) {
+    repositoryNames.forEach(deleteRepository);
+    return {
+        success: true
+    };
+}
+
+function deleteRepository(repositoryName) {
+    repoLib.delete(repositoryName);
+}
+
+function runSafely(runnable, parameter) {
+    try {
+        return runnable(parameter);
+    } catch (e) {
+        return {
+            error: 'Error while deleting repositories'
+        }
+    }
+}
