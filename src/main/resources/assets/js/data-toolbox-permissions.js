cdab09e6a4bc9317f247d247af9f7ac2c4d0e6a7
Glenn Ricaud <gri@enonic.com>
2017-10-08 13:58:04 +0200

    Node Permissions (#46)

--- src/main/resources/assets/js/data-toolbox-permissions.js
+++ src/main/resources/assets/js/data-toolbox-permissions.js
@@ -5,31 +5,27 @@ class PermissionsRoute extends DtbRoute {
         });
     }
 
+    static getPermissions() {
+        return ['READ', 'CREATE', 'MODIFY', 'DELETE', 'PUBLISH', 'READ_PERMISSIONS', 'WRITE_PERMISSIONS'];
+    }
+
     onDisplay() {
         this.refreshBreadcrumbs();
         this.retrievePermissions();
     }
     
     createLayout() {
-        const inheritanceIconArea = new RcdGoogleMaterialIconArea('arrow_upward', () => this.editPermissionInheritance()).init().
-            setTooltip('Change permission inheritance');
-        const createIconArea = new RcdGoogleMaterialIconArea('add_circle', () => this.createPermission()).init().
-            setTooltip('Create permission');
-        const deleteIconArea = new RcdGoogleMaterialIconArea('delete', () => this.deletePermissions()).init().
-            setTooltip('Delete selected permissions', RcdMaterialTooltipAlignment.RIGHT);
         this.tableCard = new RcdMaterialTableCard('Permissions').init().
             addClass('dtb-table-card-permissions').
             addColumn('Principal', {classes: ['principal']}).
-            addColumn('Read').
-            addColumn('Create').
-            addColumn('Modify').
-            addColumn('Delete').
-            addColumn('Publish').
-            addColumn('Read<br/>Perm.').
-            addColumn('Write<br/>Perm.').
-            addIconArea(inheritanceIconArea).
-            addIconArea(createIconArea, {max: 0}).
-            addIconArea(deleteIconArea, {min: 1});
+            addColumn('Permissions', {classes: ['mobile-cell']}).
+            addColumn('Read', {classes: ['non-mobile-cell']}).
+            addColumn('Create', {classes: ['non-mobile-cell']}).
+            addColumn('Modify', {classes: ['non-mobile-cell']}).
+            addColumn('Delete', {classes: ['non-mobile-cell']}).
+            addColumn('Publish', {classes: ['non-mobile-cell']}).
+            addColumn('Read<br/>Perm.', {classes: ['non-mobile-cell']}).
+            addColumn('Write<br/>Perm.', {classes: ['non-mobile-cell']});
 
         return new RcdMaterialLayout().init().
             addChild(this.tableCard);
@@ -56,13 +52,14 @@ class PermissionsRoute extends DtbRoute {
 
         this.tableCard.createRow({selectable:false}).
             addCell('..').
-            addCell('').
-            addCell('').
-            addCell('').
-            addCell('').
-            addCell('').
-            addCell('').
-            addCell('').
+            addCell('', {classes: ['mobile-cell']}).
+            addCell('', {classes: ['non-mobile-cell']}).
+            addCell('', {classes: ['non-mobile-cell']}).
+            addCell('', {classes: ['non-mobile-cell']}).
+            addCell('', {classes: ['non-mobile-cell']}).
+            addCell('', {classes: ['non-mobile-cell']}).
+            addCell('', {classes: ['non-mobile-cell']}).
+            addCell('', {classes: ['non-mobile-cell']}).
             addClass('rcd-clickable').
             addClickListener(() => setState('nodes', {repo:getRepoParameter(), branch: getBranchParameter(), path: this.getParentPath()})); //TODO
         
@@ -71,13 +68,14 @@ class PermissionsRoute extends DtbRoute {
             result.success._permissions.forEach((accessControlEntry) => {
                 this.tableCard.createRow().
                     addCell(accessControlEntry.principal, {classes: ['principal']}).
-                    addCell(this.createPermissionIcon(accessControlEntry, 'READ'), {icon: true}).
-                    addCell(this.createPermissionIcon(accessControlEntry, 'CREATE'), {icon: true}).
-                    addCell(this.createPermissionIcon(accessControlEntry, 'MODIFY'), {icon: true}).
-                    addCell(this.createPermissionIcon(accessControlEntry, 'DELETE'), {icon: true}).
-                    addCell(this.createPermissionIcon(accessControlEntry, 'PUBLISH'), {icon: true}).
-                    addCell(this.createPermissionIcon(accessControlEntry, 'READ_PERMISSIONS'), {icon: true}).
-                    addCell(this.createPermissionIcon(accessControlEntry, 'WRITE_PERMISSIONS'), {icon: true}).
+                    addCell(this.createPermissionResume(accessControlEntry), {classes: ['mobile-cell']}).
+                    addCell(this.createPermissionIcon(accessControlEntry, 'READ'), {icon: true, classes: ['non-mobile-cell', 'permission']}).
+                    addCell(this.createPermissionIcon(accessControlEntry, 'CREATE'), {icon: true, classes: ['non-mobile-cell', 'permission']}).
+                    addCell(this.createPermissionIcon(accessControlEntry, 'MODIFY'), {icon: true, classes: ['non-mobile-cell', 'permission']}).
+                    addCell(this.createPermissionIcon(accessControlEntry, 'DELETE'), {icon: true, classes: ['non-mobile-cell', 'permission']}).
+                    addCell(this.createPermissionIcon(accessControlEntry, 'PUBLISH'), {icon: true, classes: ['non-mobile-cell', 'permission']}).
+                    addCell(this.createPermissionIcon(accessControlEntry, 'READ_PERMISSIONS'), {icon: true, classes: ['non-mobile-cell', 'permission']}).
+                    addCell(this.createPermissionIcon(accessControlEntry, 'WRITE_PERMISSIONS'), {icon: true, classes: ['non-mobile-cell', 'permission']}).
                     setAttribute('principal', accessControlEntry.principal);
             });
         }
@@ -86,15 +84,52 @@ class PermissionsRoute extends DtbRoute {
     setInheritPermission(inheritsPermissions) { 
         this.tableCard.setTitle('Permissions' + (inheritsPermissions ? ' (inherited)' : ''));
     }
-    
+
     createPermissionIcon(accessControlEntry, permission ) {
-        if (accessControlEntry.allow.indexOf(permission) !== -1) {
-            return new RcdGoogleMaterialIconArea('check').init();
-        }
         if (accessControlEntry.deny.indexOf(permission) !== -1) {
             return new RcdGoogleMaterialIconArea('block').init();
         }
+        if (accessControlEntry.allow.indexOf(permission) !== -1) {
+            return new RcdGoogleMaterialIconArea('check').init();
+        }
         return '';
+    }
+    
+    createPermissionResume(accessControlEntry) {
+        if (this.hasOnlyPermissions(accessControlEntry, ['READ'])) {
+            return 'Can read';
+        }
+        if (this.hasOnlyPermissions(accessControlEntry, ['READ', 'CREATE', 'MODIFY', 'DELETE'])) {
+            return 'Can write';
+        }
+        if (this.hasOnlyPermissions(accessControlEntry, ['READ', 'CREATE', 'MODIFY', 'DELETE', 'PUBLISH'])) {
+            return 'Can publish';
+        }
+        if (this.hasOnlyPermissions(accessControlEntry, ['READ', 'CREATE', 'MODIFY', 'DELETE', 'PUBLISH', 'READ_PERMISSIONS', 'WRITE_PERMISSIONS'])) {
+            return 'Full access';
+        }
+        return 'Custom...';
+    }
+
+    hasOnlyPermissions(accessControlEntry, permissions) {
+        let otherPermissions = PermissionsRoute.getPermissions();
+        for (let permission of permissions) {
+            if (!this.hasPermission(accessControlEntry, permission)) {
+                return false;
+            }
+            otherPermissions.splice(otherPermissions.indexOf(permission), 1);
+        }
+        for (let otherPermission of otherPermissions) {
+            if (this.hasPermission(accessControlEntry, otherPermission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    hasPermission(accessControlEntry, permission) {
+        return accessControlEntry.deny.indexOf(permission) === -1 && 
+               accessControlEntry.allow.indexOf(permission) !== -1;
     }
     
     createPermission() {
@@ -136,7 +171,7 @@ class PermissionsRoute extends DtbRoute {
                 new RcdMaterialBreadcrumb(branchName, () => setState('nodes',{repo: repositoryName, branch: branchName})).init()]);
 
         
-        this.breadcrumbsLayout.addBreadcrumb(new RcdMaterialBreadcrumb(path === '/' ? 'root/permissions' : 'root', path === '/' ? undefined :
+        this.breadcrumbsLayout.addBreadcrumb(new RcdMaterialBreadcrumb(path === '/' ? 'root!permissions' : 'root', path === '/' ? undefined :
                                                                           () => setState('nodes', {repo: repositoryName, branch: branchName, path: '/'})).init());
 
         if (path === '/') {
@@ -149,7 +184,7 @@ class PermissionsRoute extends DtbRoute {
             pathElements.forEach((subPathElement, index, array) => {
                 currentPath += '/' + subPathElement;
                 const constCurrentPath = currentPath;
-                this.breadcrumbsLayout.addBreadcrumb(new RcdMaterialBreadcrumb(index < array.length - 1 ?  subPathElement : subPathElement + '/permissions', index < array.length - 1
+                this.breadcrumbsLayout.addBreadcrumb(new RcdMaterialBreadcrumb(index < array.length - 1 ?  subPathElement : subPathElement + '!permissions', index < array.length - 1
                     ? (() => setState('nodes', {repo: repositoryName, branch: branchName, path: constCurrentPath}))
                     : undefined).init());
             });
@@ -158,41 +193,12 @@ class PermissionsRoute extends DtbRoute {
     }
 
     displayHelp() {
-        const definition = 'A Node represents a single storable entity of data. ' +
-                           'It can be compared to a row in sql or a document in document oriented storage models.<br/>' +
-                           'See <a class="rcd-material-link" href="http://xp.readthedocs.io/en/6.10/developer/node-domain/nodes.html">Nodes</a> for more information. ';
+        const definition = 'Permissions are granted to principals (users, groups and roles) on a per-node basis. This means that changing a principal’s permissions for one node does not affect that principal’s permissions for other node.' +
+                           'See <a class="rcd-material-link" href="http://xp.readthedocs.io/en/6.10/admin/contentstudio/content-security.html">Content Security</a> for more information. ';
 
-        const structureDefinition = 'This tool represent nodes in a tree structure. ' +
-                                    'While this solution is adapted to repositories like cms-repo or system-repo, ' +
-                                    'it may be problematic for custom repositories or for nodes with too many children. ' +
-                                    'Node search for all paths will be available in the future, but if this representation is blocking we recommend using the tool ' +
-                                    '<a class="rcd-material-link" href="https://market.enonic.com/vendors/runar-myklebust/repoxplorer">repoXPlorer</a>.';
-
-        const viewDefinition = 'The view lists in a table all the direct children nodes of the current node (or the root node for a branch). Click on a row to display its direct children.';
-        new HelpDialog('Permissions', [definition, structureDefinition, viewDefinition]).
+        const viewDefinition = 'The view lists in a table all the permissions of the current node. Creation, modification and deletion of permissions will be provided in an ulterior version.';
+        new HelpDialog('Permissions', [definition, viewDefinition]).
         init().
-        addActionDefinition({
-            iconSrc: config.assetsUrl + '/icons/export-icon.svg',
-            definition: 'Export the selected node into $XP_HOME/data/export/[export-name]. The display will switch to the Exports view.'
-        }).
-        addActionDefinition({
-            iconSrc: config.assetsUrl + '/icons/import-icon.svg',
-            definition: 'Import previously exported nodes as children under the current node (or as root node)'
-        }).
-        addActionDefinition({
-            iconName: 'filter_list',
-            definition: 'Filter the nodes based on a query expression. ' +
-                        'Example: "_id = \'role:system.admin"\'. ' +
-                        'See <a class="rcd-material-link" href="http://xp.readthedocs.io/en/6.10/reference/query-language.html#compareexpr">Query language</a> for more information.'
-        }).
-        addActionDefinition({
-            iconName: 'sort',
-            definition: 'Sort the nodes based on an expression. ' +
-                        'The sorting expression is composed of a node field to sort on and the direction: ascending or descending.' +
-                        'Examples: "_timestamp DESC", "_name ASC"'
-        }).
-        addActionDefinition({iconName: 'delete', definition: 'Delete the selected nodes.'}).
-        addActionDefinition({iconName: 'info', definition: 'Display the node content.'}).
         open();
     }
 }
