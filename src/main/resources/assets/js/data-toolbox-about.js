ac990582683025f40be9cc042e83ddf28b987a5a
Glenn Ricaud <gri@enonic.com>
2018-09-09 21:16:05 +0200

    About page #88

--- src/main/resources/assets/js/data-toolbox-about.js
+++ src/main/resources/assets/js/data-toolbox-about.js
@@ -32,6 +32,7 @@ class AboutRoute extends DtbRoute {
             if (handleResultError(result)) {
                 const displayMarketCallback = () =>
                     window.open('https://market.enonic.com/vendors/glenn-ricaud/systems.rcd.enonic.datatoolbox', '_blank');
+                const githubRepoCallback = () => window.open('https://github.com/GlennRicaud/data-toolbox-app')
                 const reportIssueCallback = () => window.open('https://github.com/GlennRicaud/data-toolbox-app/issues/new?body=Enonic XP: ' +
                                                               result.success.app.xpVersion + ' %0A' +
                                                               'Data Toolbox: ' + result.success.app.version + '%0A%0A' +
@@ -40,8 +41,10 @@ class AboutRoute extends DtbRoute {
                     .addRow('Version ' + result.success.app.version, 'Running on Enonic XP ' + result.success.app.xpVersion)
                     .addRow('Display market page ', null,
                         {callback: displayMarketCallback, icon: new RcdGoogleMaterialIcon('open_in_new').init()})
+                    .addRow('Github repository', 'Feel free to star ;)',
+                        {callback: githubRepoCallback, icon: new RcdImageIcon(config.assetsUrl + '/icons/github.svg').init()})
                     .addRow('Report an issue ', null,
-                        {callback: reportIssueCallback, icon: new RcdGoogleMaterialIcon('open_in_new').init()});
+                        {callback: reportIssueCallback, icon: new RcdImageIcon(config.assetsUrl + '/icons/github.svg').init()});
             }
         }).fail(handleAjaxError).always(() => {
             infoDialog.close();
